04125c3fd2a25c3b3541066aa9425ee2c0ca83c0
GiridharaGopal-for-all <gopalgiridhara@gmail.com>
2025-07-30 21:34:35 +0530

    remove1

--- tests/Green_Kart/conftest.py
+++ tests/Green_Kart/conftest.py
@@ -1,30 +1,28 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.edge.service import Service as EdgeService
 
 
-@pytest.fixture(params=["Chrome"],scope="class",autouse=True)
+@pytest.fixture(params=["Chrome"], scope="class", autouse=True)
 def browsers(request):
-    browser=request.param
-    if browser=="Chrome":
-        driver= webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    browser = request.param
+    if browser == "Chrome":
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")  # Run in headless mode
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-gpu")  # Optional
+        chrome_options.add_argument("--remote-debugging-port=9222")  # Optional
 
-    elif browser=="Edge":
-        service = EdgeService(executable_path=r"C:\Users\Devils Den\msedgedriver.exe")
-        driver = webdriver.Edge(service=service)
-
-    else :
-            driver=webdriver.Firefox(service=Service(GeckoDriverManager().install()))
-
-    driver.implicitly_wait(12)
-    driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
-    driver.maximize_window()
-    request.cls.driver = driver
-    yield
-    driver.close()
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+        driver.maximize_window()
+        request.cls.driver = driver
+        yield
+        driver.quit()
 
 # @pytest.fixture(params=testdata.vegetables)
 # def test_data(request):
